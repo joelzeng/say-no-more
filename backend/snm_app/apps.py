afd5306dea4f4c5fa22ff3600422ac3b2e1349e6
Jaydon <lau.jaydon@gmail.com>
2019-09-05 22:55:59 -0400

    add django project and virtualenv

---
+++ backend/snm_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SnmAppConfig(AppConfig):
+    name = 'snm_app'
---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jaydonlau/projects/say-no-more/virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
